34b531b590860486ec109c498a8784980aa1a38b
yntsuji <yukitsujiamk@gmail.com>
2026-04-08 20:54:13 +0300

    Assignment 4 initialized

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cc-mod4-backend
+
+# start
+
+flask --app api run --debug
+waitress-serve --host 127.0.0.1 api:app
+python app.py
+
+
---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, hi World!</p>"
+
+@app.route("/api", methods=["POST"])
+def sentiment():
+    body_data = request.get_json()
+    print("Body data:")
+    print(body_data["data"])
+    return {"message": "hello from api"}
