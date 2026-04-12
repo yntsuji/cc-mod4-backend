d3aa196a5e3c3b0776865783ea43871e5e3235da
yntsuji <yukitsujiamk@gmail.com>
2026-04-12 19:24:37 +0300

    testi

--- api.py
+++ api.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
+import pickle
+
 
 app = Flask(__name__)
 
+
+with open("sentiment_model_v1.pkl", "rb") as file:
+    model = pickle.load(file)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, hi World hjkl 45!</p>"
+    return "<p>Hello, hi World hjkl 45! testi</p>"
 
-@app.route("/api", methods=["POST"])
-def sentiment():
+# @app.route("/api", methods=["POST"])
+# def sentiment():
     body_data = request.get_json()
     print("Body data:")
     print(body_data["data"])
     return {"message": "hello from api"}
+
